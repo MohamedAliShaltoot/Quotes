685818866a025c9d0705b1d4e43ab411f3e6765a
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-01-30 17:38:01 +0200

    add showToast reusable function

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:quotes/core/utils/app_colors.dart';
 import 'package:quotes/core/utils/app_strings.dart';
 
-class Constants{
- static void showErrorDialog({ required BuildContext context , required String msg}){
-   showDialog(context: context, builder: (context)=> CupertinoAlertDialog(
-     title: Text(msg),
-     actions: [
-       TextButton(onPressed: (){
-         Navigator.pop(context);
-       }, child:Text(AppStrings.okDialogButton))
-     ],
-   ));
-    
-    
+class Constants {
+  static void showErrorDialog(
+      {required BuildContext context, required String msg}) {
+    showDialog(
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+              title: Text(msg),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(AppStrings.okDialogButton))
+              ],
+            ));
+  }
+
+  static showToast(
+      {required BuildContext context, required String msg,Color? color,ToastGravity? gravity}
+      ){
+    Fluttertoast.showToast(
+      toastLength: Toast.LENGTH_LONG,
+        msg: msg,
+      gravity: gravity ?? ToastGravity.BOTTOM,
+      backgroundColor: color ?? AppColors.primaryColor
+    );
   }
 }
